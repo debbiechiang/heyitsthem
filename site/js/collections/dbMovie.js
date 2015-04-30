67b8b1b947c8491e4cd3d4ac0187e4276399084a
debbiechiang <sangoire@gmail.com>
2015-04-30 16:02:37 -0400

    Added a clearer naming structure

--- site/js/collections/dbMovie.js
+++
@@ -1,8 +0,0 @@
-// site/js/collections/dbMovie.js
-
-var app = app || {};
-
-app.DBMovieCollection = Backbone.Collection.extend({
-	model: app.Movie, 
-	url: '/api/media'
-});
--- site/js/lib/mvc.js
+++ site/js/lib/mvc.js
@@ -95,14 +95,6 @@ app.Cast = Backbone.Collection.extend({
 		} 
 	}
 })
-// site/js/collections/dbMovie.js
-
-var app = app || {};
-
-app.DBMovieCollection = Backbone.Collection.extend({
-	model: app.Movie, 
-	url: '/api/media'
-});
 // site/js/collections/formCollection.js
 
 var app = app || {};
